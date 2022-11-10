a83a434800e6af872f187c7537574d3b42802b06
len4ias <117491580+len4ias@users.noreply.github.com>
2022-11-09 17:21:42 -0800

    Update ReviewClass.java
    Main method and review method removed

--- src/review/ReviewClass.java
+++ src/review/ReviewClass.java
@@ -2,13 +2,6 @@ package review;
 
 public class ReviewClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void test() {
-		System.out.println("Thismethod is on reviewS branch");
-	}
+	
 	
 }
